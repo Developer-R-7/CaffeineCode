f8ac811e55e422338ce1745d78d5f77519ab1511
rushi patel <rushinasa06@gmail.com>
2021-07-25 20:36:18 +0530

    done styling proer

--- blog/models.py
+++ blog/models.py
@@ -7,17 +7,22 @@ from hitcount.models import HitCountMixin, HitCount
 from django.contrib.contenttypes.fields import GenericRelation
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=75)
     blog_snipet = models.CharField(max_length=200)
     title_tag = models.CharField(max_length=20)
     author = models.ForeignKey(User,on_delete=models.CASCADE)
     body = RichTextField(null=True,blank=True)
     date_published = models.DateField(auto_now_add=True)
-    entry_img = models.URLField(max_length=500)
+    entry_img = models.URLField()
     tags = TaggableManager()
     modified = models.DateTimeField(auto_now=True)
     hit_count_generic = GenericRelation(HitCount, object_id_field='object_pk',related_query_name='hit_count_generic_relation')
     author_conculsion = models.CharField(max_length=300,default="No Author Review")
     readtime_min = models.IntegerField(default=0) 
     def __str__(self):
-        return self.title + '|' + str(self.author)
+        return self.title + ' | ' + str(self.author)
+
+class Newsletter(models.Model):
+    email = models.EmailField()
+    def __str__(self):
+        return self.email
--- blog/views.py
+++ blog/views.py
@@ -12,10 +12,10 @@ from hitcount.views import HitCountDetailView
 class HomeView(ListView):
     model = Post
     template_name = "blog/index.html"
-    ordering = ['-hit_count_generic__hits','-date_published']
+    ordering = ['-date_published','-hit_count_generic__hits']
     paginate_by = 5
     def get_context_data(self,**kwargs):
-        most_used_tags = Post.tags.most_common()[:6]
+        most_used_tags = Post.tags.most_common()[:9]
         context = super(HomeView,self).get_context_data(**kwargs)
         context['most_used_tag'] = most_used_tags
         context['trending_post'] = Post.objects.order_by('-hit_count_generic__hits')[:5]
@@ -36,8 +36,6 @@ def post_by_tags(request,tag_slug):
     posts = paginator.get_page(page)
     most_view = Post.objects.order_by('-date_published')[:5]
     return render(request, 'blog/blogtag.html', {'page': page,'posts': posts,'tag': tag,'most_view':most_view,'most_tags':most_used_tags})
-
-
 
 class ArticleDetailView(HitCountDetailView):
     model = Post
