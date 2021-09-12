4e945015521bb01e2c8c97b0ac72c228decfd7f9
rushi patel <rushinasa06@gmail.com>
2021-09-12 12:47:37 +0530

    succefully done celery email asynchronous task on redis server

--- IndexHome/task.py
+++ IndexHome/task.py
@@ -11,5 +11,10 @@ from celery.decorators import task
 
 @task()
 def SendOTP(email,otp):
-    send_mail('HELLO TEST MAIL','THIS MAIL IS SENT BY CELERY',settings.EMAIL_HOST_USER,[email],fail_silently=True)
+    #send_mail('HELLO TEST MAIL','THIS MAIL IS SENT BY CELERY',settings.EMAIL_HOST_USER,[email],fail_silently=True)
+    html_content = render_to_string("IndexHome/email.html",{'otp':otp,'email':email})
+    text_content = strip_tags(html_content)
+    email_con = EmailMultiAlternatives('Verfication Code - CaffeineCode',text_content,settings.EMAIL_HOST_USER,[email]) 
+    email_con.attach_alternative(html_content,"text/html")
+    email_con.send()
     return "Done"
