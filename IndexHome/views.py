7dcd164447de5ec7ef89311c97b27084066f00f4
rushi patel <rushinasa06@gmail.com>
2021-12-23 23:44:47 +0530

    done error logger part

--- IndexHome/views.py
+++ IndexHome/views.py
@@ -8,6 +8,7 @@ from django.views.decorators.cache import never_cache
 from blog.models import Post,Category
 from api.Users.UserManager import UserAPI
 from api.Profiles.ProfileManager import profile_manager
+from api.Config.error import error_message
 # CLIENT-SIDE FUNCTIONS
 def check_user(request):
     username = request.GET.get('username', None)
@@ -22,10 +23,10 @@ def verify(request, mail_hash, id):
     try:
         profile_connector = profile_manager()
         get_user = profile_connector.search_user_with_id(id)
-        decrypt_email = profile_connector.get_decrypted_string(mail_hash.encode('utf-8'),id)  # this is a bytes
+        decrypt_email = profile_connector.get_decrypted_string(mail_hash.encode('utf-8'),id)
         is_user_verify = profile_connector.is_user_verify(decrypt_email)
     except:
-        return render(request, "IndexHome/error.html", {'error': "Unauthorized Access "})
+        return render(request, "config/error.html", {'error': error_message[0]})
     if request.method == "POST":
         USER_OTP_IN = request.POST.get('Passcode')
         if profile_connector.verify_otp(id, USER_OTP_IN):
@@ -36,17 +37,17 @@ def verify(request, mail_hash, id):
                 # login user here
                 return redirect("/dashboard/home")
             except:
-                return render(request, "IndexHome/error.html", {'error': "Verification failed user not verified Contact Support!"})
+                return render(request, "config/error.html", {'error': error_message[15]})
         else:
             if profile_connector.get_fail(id) < 3:
                 profile_connector.add_fail_request(id)
                 return render(request, "IndexHome/verify.html", {'otp_FAILED': True, 'mail': mail_hash, 'email': decrypt_email.decode(), "id": id})
             else:
                 profile_connector.reset_fail(id)
-                return render(request, 'IndexHome/error.html', {'error': "Max OTP requested. Verification failed "})
+                return render(request, 'config/error.html', {'error': error_message[13]})
     else:
         if is_user_verify:
-            return render(request, "IndexHome/error.html", {'error': "User already verify!"})
+            return render(request, "config/error.html", {'error': error_message[14]})
         else:
             try:
                 get_request_from_main = request.session['main_verify']
@@ -66,14 +67,14 @@ def resend_otp(request, mail_hash, acc_id, request_otp):
             get_user = profile_connector.search_user_with_id(acc_id)
             email = profile_connector.get_decrypted_string(mail_hash.encode('utf-8'),acc_id)
         except:
-            return render(request, "IndexHome/error.html", {'error': "Unauthorized request send"})
+            return render(request, "config/error.html", {'error':error_message[0]})
         if get_user.resend_request < 3:
             profile_connector.generate_only_otp(acc_id)
             profile_connector.resend_request(acc_id)
             return render(request, "IndexHome/verify.html", {'mail': mail_hash, 'email': email.decode('utf-8'), 'resend_request': True, 'id': acc_id})
         else:
             profile_connector.reset_resend(acc_id)
-            return render(request, 'IndexHome/error.html', {'error': "Max OTP requested. Verification failed "})
+            return render(request, 'config/error.html', {'error': error_message[1]})
     else:
         raise Exception("false request")
 
@@ -90,10 +91,10 @@ def signin(request):
                 sign_in_password = request.POST.get('password', 'default')
                 user_connector = UserAPI()
             except:
-                return render('IndexHome/error.html', {"error": 'Bypass blocked', "status": "high"})
+                return render('config/error.html', {"error": error_message[2]})
     
             if user_connector.is_user_has_account(sign_in_email) is False:
-                return render(request, 'IndexHome/login.html', {"error": "You don't have account linked with this mail","create_account":True})
+                return render(request, 'IndexHome/login.html', {"error": error_message[3],"create_account":True})
             else:
                 try:
                     user = auth.authenticate(username=user_connector.get_username(sign_in_email), password=sign_in_password)
@@ -108,11 +109,11 @@ def signin(request):
                             get_user = user_connector.search_user_with_account_mail(sign_in_email)
                             ver_req = user_connector.get_encrypted_string(sign_in_email,user_connector.get_key(get_user.account_id))
                             request.session['main_verify'] = True
-                            return render(request, 'IndexHome/login.html', {"error": "Account not verified", "verify_request": True, "mail_en": ver_req.decode('utf-8'), "id": get_user.account_id})
+                            return render(request, 'IndexHome/login.html', {"error": error_message[4], "verify_request": True, "mail_en": ver_req.decode('utf-8'), "id": get_user.account_id})
                     else:
-                        return render(request, 'IndexHome/login.html', {"error": "Incorrect email or password"})
+                        return render(request, 'IndexHome/login.html', {"error": error_message[5]})
                 except:
-                    return render(request, 'IndexHome/error.html', {'error': "Error Signin ,Please contact support.", "status": "high"})
+                    return render(request, 'config/error.html', {'error': error_message[6]})
         else:
             try:
                 pk_value = request.GET.get("blog-redirect-id")
@@ -135,18 +136,18 @@ def signup(request):
                 data_pass = request.POST.get("password")
                 user_connector = UserAPI()
             except:
-                return render(request, "IndexHome/error.html", {"error": "By Pass blocked!", "status": "high"})
+                return render(request, "config/error.html", {"error": error_message[2]})
             try:
                 if user_connector.is_user_has_account(data_email):
-                    return render(request, 'IndexHome/signup.html', {"error": "You Already Have account linked with this mail"})
+                    return render(request, 'IndexHome/signup.html', {"error": error_message[7]})
                 else:
                     if user_connector.check_same_username(data_name):
-                        return render(request, 'IndexHome/error.html', {"error": "Unauthorized access"})
+                        return render(request, 'config/error.html', {"error": error_message[0]})
                     else:
                         get_params = user_connector.create_account(data_name,data_email,data_pass)
                         return redirect('/verify/{}/{}/'.format(get_params[0].decode('utf-8'),get_params[1]))
             except:
-                return render(request,'IndexHome/error.html',{'error':"Oops! Somethings went wrong ,Please contact support."})
+                return render(request,'config/error.html',{'error':error_message[8]})
         else:
             return render(request, 'IndexHome/signup.html')
 
@@ -169,11 +170,11 @@ def forgot(request):
                     request.session['email'] = mail_to_request
                     return render(request, "IndexHome/forgot-final.html")
                 else:
-                    return render(request, 'IndexHome/forgot.html', {"warning": True, "message": "First verify your account to request forgot-password"})
+                    return render(request, 'IndexHome/forgot.html', {"warning": True, "message": error_message[9]})
             else:
-                return render(request, "IndexHome/forgot.html", {"warning": True, "message": "No user with this email"})
+                return render(request, "IndexHome/forgot.html", {"warning": True, "message": error_message[3]})
         except:
-            return render(request, "IndexHome/error.html", {"error": "Failed to make forgot password request"})
+            return render(request, "config/error.html", {"error": error_message[16]})
     else:
         return render(request, "IndexHome/forgot.html")
 
@@ -181,7 +182,7 @@ def forgot_final(request):
     try:
         mail = request.session['email']
     except KeyError:
-        return render(request, 'IndexHome/error.html', {"error": "Unauthorized request"})
+        return render(request, 'config/error.html', {"error":error_message[0]})
     if request.method == "POST":
         try:
             get_code = request.POST.get("code")
@@ -190,11 +191,11 @@ def forgot_final(request):
             get_user = user_connector.search_user_with_account_mail(mail)
             if user_connector.verify_otp(get_user.account_id,get_code):
                 user_connector.set_user_password(mail,get_new_pass)
-                return render(request, 'IndexHome/error.html', {"error": "Your Password Changed successful"})
+                return render(request, 'IndexHome/index.html', {"toast":True,"toast_mssg": error_message[10]})
             else:
-                return render(request, 'IndexHome/forgot-final.html', {"error": "Incorrect Verifcation code"})
+                return render(request, 'IndexHome/forgot-final.html', {"error": error_message[11]})
         except:
-            return render(request, 'IndexHome/error.html', {"error": "Something went wrong Please contact support"})
+            return render(request, 'config/error.html', {"error": error_message[12]})
     else:
         return render(request, 'IndexHome/forgot-final.html')
 
@@ -208,7 +209,7 @@ def playground_timer(request):
             obj.save()
             return render(request,'IndexHome/playground.html',{"toast":True,"toast_mssg":"Notify register successful!"})
         except:
-            return render(request,'IndexHome/error.html',{"error":"Notify Failed!!"})
+            return render(request,'config/error.html',{"error":error_message[17]})
     else:
         return render(request, "IndexHome/playground.html")
 
---
+++ api/Config/error.py
@@ -0,0 +1,22 @@
+error_message = [
+
+    "Unauthorized request",#0
+    "Maximum verification-code requested , try signin again to regenerate verify request",#1
+    "Bypass blocked",#2
+    "You don't have account linked with this email",#3
+    "Account not verified",#4
+    "Incorrect email or password",#5
+    "Oops! we encoutered a error in signin , please contact support",#6
+    "You already have account linked with this email",#7
+    "Oops! we encoutered a error in signup , please contact support",#8
+    "Account must be verified to request forgot-password",#9
+    "Password Changed successful",#10
+    "Incorrect Verifcation code",#11
+    "Oops! we encoutered a error in forgot password request , please contact support",#12
+    "Maximum verification-code failed , try signin again to regenerate verify request",#13
+    "User already verify!",#14
+    "Oops! we encoutered a error in verification , please contact support",#15
+    "Oops! we encoutered a error in generating forgot password request, please contact support",#16
+    "Notify Failed!!"#17
+
+]
